14f9838a499b248b645ebf0df959075cae054ef2
Zaiste <oh@zaiste.net>
2022-05-26 13:40:49 +0200

    feat: add middleware for HTTP method selection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,7 @@
 import type { Middleware } from "./types";
+import type { HTTPMethod } from "./";
+
+import { Response } from './response';
 
 export const asJSON: Middleware = (handler) => async (request) => {
   const response = await handler(request);
@@ -28,4 +31,17 @@ export const asHTML: Middleware = (handler) => async (request) => {
   };
 
   return newResponse;
+}
+
+
+export const withMethod = (...methods: HTTPMethod[]): Middleware => (handler) => async (request) => {
+  const { method } = request;
+
+  if (! methods.includes(method)) {
+    return Response.MethodNotAllowed();
+  }
+
+  const response = await handler(request);
+  
+  return response;
 }
