2fb1d6a0818fdf8da6aaf416f8a8295bbe1287da
Zaiste <oh@zaiste.net>
2022-08-08 10:49:48 +0200

    Narrow the type for values of headers
    Fixes #13

--- src/types.ts
+++ src/types.ts
@@ -1,111 +1,113 @@
 import http from 'http';
-import { HTTPMethod } from './';
 import { Readable } from 'stream';
+import { HTTPMethod } from './';
 import { Response } from './response';
 
 export type BodyInit = string | object | Readable;
 export type HeadersInit = Record<string, string>;
 
 export interface Params {
-	[name: string]: any
+	[name: string]: any;
 }
 
 interface AnyValue {
-	[name: string]: any
+	[name: string]: any;
 }
 export interface Request<I = Params> {
-	params: I
+	params: I;
 	headers: {
-		[name: string]: any
-	}
+		[name: string]: string | undefined;
+	};
 	files?: {
 		[name: string]: {
-			name: string
-			length: number
-			data: any
-			encoding: string
-			mimetype: string
-		}
-	}
-	cookies?: object
-	user?: unknown
+			name: string;
+			length: number;
+			data: any;
+			encoding: string;
+			mimetype: string;
+		};
+	};
+	cookies?: object;
+	user?: unknown;
 	host: string;
-	url: string
-	method: HTTPMethod
-	path?: string
-	format?: string
-	body: http.IncomingMessage
-	rawBody?: string
-	response: http.ServerResponse
-	context: AnyValue
+	url: string;
+	method: HTTPMethod;
+	path?: string;
+	format?: string;
+	body: http.IncomingMessage;
+	rawBody?: string;
+	response: http.ServerResponse;
+	context: AnyValue;
 }
 
 export type MaybePromise<T> = T | Promise<T> | PromiseLike<T>;
 
-export type Handler<I = Params, O extends BodyInit = BodyInit> = (request: Request<I>) => MaybePromise<Response<O>>;
+export type Handler<I = Params, O extends BodyInit = BodyInit> = (
+	request: Request<I>,
+) => MaybePromise<Response<O>>;
 export type Pipeline = [...Middleware[], Handler];
 export type ReversedPipeline = [Handler, ...Middleware[]];
 
 export interface Meta {
-	summary?: string
-	description?: string
-	parameters?: Array<any>
-	responses?: Object
+	summary?: string;
+	description?: string;
+	parameters?: Array<any>;
+	responses?: Object;
 }
 
-export type Middleware = (handler: Handler) => (request: Request) => MaybePromise<Response>
+export type Middleware = (handler: Handler) => (request: Request) => MaybePromise<Response>;
 
 // export interface RoutePath {
 //   [name: HTTPMethod]: any
 // }
 
 export interface RoutePaths {
-	[name: string]: any
+	[name: string]: any;
 }
 
 export interface RouteOptions {
-	middleware?: Middleware[]
-	meta?: Meta
+	middleware?: Middleware[];
+	meta?: Meta;
 }
 
 interface RouteParams {
-	GET?: Handler
-	POST?: Handler
-	PUT?: Handler
-	PATCH?: Handler
-	DELETE?: Handler
-	middleware?: Middleware[]
-	meta?: Meta
+	GET?: Handler;
+	POST?: Handler;
+	PUT?: Handler;
+	PATCH?: Handler;
+	DELETE?: Handler;
+	middleware?: Middleware[];
+	meta?: Meta;
 }
 
 export type Route = [string, RouteParams, Route?];
 
-export type Routes = Route[]
+export type Routes = Route[];
 
 export interface Context {
-	request: http.IncomingMessage
-	response: http.ServerResponse
-	params: Params
-	url: string
-	method: string
-	headers?: Params
-	cookies?: Object
-	format?: string
-	files?: Object
+	request: http.IncomingMessage;
+	response: http.ServerResponse;
+	params: Params;
+	url: string;
+	method: string;
+	headers?: Params;
+	cookies?: Object;
+	format?: string;
+	files?: Object;
 }
 
 export interface HandlerParams {
-	handler: Handler
-	names: string[]
+	handler: Handler;
+	names: string[];
 }
 
 export interface HandlerParamsMap {
-	[method: string]: HandlerParams
+	[method: string]: HandlerParams;
 }
 
 export interface KeyValue {
-	name: string
-	value: string
+	name: string;
+	value: string;
 }
 
-export { Response }
+export { Response };
